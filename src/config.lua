52782f93bb60cd6ed92b23556ded47e84c6f2520
GuglioIsStupid <bradsbluetab@gmail.com>
2025-06-16 17:36:39 -0400

    hol;y shit ve 2

---
+++ src/config.lua
@@ -0,0 +1,10 @@
+return {
+    WindowWidth = 1280,
+    WindowHeight = 720,
+    GameWidth = 1280,
+    GameHeight = 720,
+
+    Title = "Friday Night Funkin' - Vanilla Engine {version}",
+    Version = "1.0.0",
+    Identity = "VanillaEngine"
+}
