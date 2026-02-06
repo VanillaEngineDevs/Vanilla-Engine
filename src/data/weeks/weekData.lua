6067e3cba2582955b6d60e70c10a03d350974ab8
GuglioIsStupid <guglioisbusiness@gmail.com>
2026-02-06 06:51:06 -0500

    Oops deleted these on accident

---
+++ src/data/weeks/weekData.lua
@@ -0,0 +1,12 @@
+l.lreturn {
+    require "weeks.tutorial",
+    require "weeks.week1",
+    require "weeks.week2",
+    require "weeks.week3",
+    require "weeks.week4",
+    require "weeks.week5",
+    require "weeks.week6",
+    require "weeks.week7",
+    require "weeks.weekend1",
+    require "weeks.test",
+}
---
+++ src/data/weeks/weekMeta.lua
@@ -0,0 +1,80 @@
+return { -- Add/remove weeks here
+    {
+        "Tutorial",
+        {
+            {"Tutorial"}
+        }
+    },
+    {
+        "Week 1",
+        {
+            {"Bopeebo",   erect=true, pico=true},
+            {"Fresh",     erect=true, pico=true},
+            {"Dadbattle", erect=true, pico=true}
+        }
+    },
+    {
+        "Week 2",
+        {
+            {"Spookeez",  erect=true, pico=true},
+            {"South",     erect=true, pico=true},
+            {"Monster"}
+        }
+    },
+    {
+        "Week 3",
+        {
+            {"Pico",        erect=true, pico=true},
+            {"Philly Nice", erect=true, pico=true},
+            {"Blammed",     erect=true, pico=true}
+        }
+    },
+    {
+        "Week 4",
+        {
+            {"Satin Panties", erect=true},
+            {"High",          erect=true},
+            {"M.I.L.F"}
+        }
+    },
+    {
+        "Week 5",
+        {
+            {"Cocoa",  erect=true, pico=true},
+            {"Eggnog", erect=true, pico=true},
+            {"Winter Horrorland"}
+        }
+    },
+    {
+        "Week 6",
+        {
+            {"Senpai", erect=true, pico=true},
+            {"Roses",  erect=true, pico=true},
+            {"Thorns", erect=true, pico=false}
+        }
+    },
+    {
+        "Week 7",
+        {
+            {"Ugh",   erect=true,  pico=true},
+            {"Guns",  erect=false, pico=true},
+            {"Stress",erect=false, pico=true}
+        }
+    },
+    {
+        "Weekend 1",
+        {
+            {"Darnell", diffs = {{"easy", nil, nil, "-pico"}, {"normal", nil, nil, "-pico"}, {"hard", nil, nil, "-pico"}, {"erect", "-erect", nil, "-pico"}, {"nightmare", "-erect", nil, "-pico"}, {"BF Easy", "-bf", "easy", "-bf"}, {"BF Normal", "-bf", "normal", "-bf"}, {"BF Hard", "-bf", "hard", "-bf"}}},
+            {"Lit Up", diffs = {{"easy", nil, nil, "-pico"}, {"normal", nil, nil, "-pico"}, {"hard", nil, nil, "-pico"}, {"BF Easy", "-bf", "easy", "-bf"}, {"BF Normal", "-bf", "normal", "-bf"}, {"BF Hard", "-bf", "hard", "-bf"}}},
+            {"2hot", diffs = {{"easy", nil, nil, "-pico"}, {"normal", nil, nil, "-pico"}, {"hard", nil, nil, "-pico"}}},
+            {"Blazin", diffs = {{"easy", nil, nil, "-pico"}, {"normal", nil, nil, "-pico"}, {"hard", nil, nil, "-pico"}}},
+            {"Cutscene", show=false}
+        }
+    },
+    {
+        "Test",
+        {
+            {"Test", diffs = {{"normal", ""}}}, -- Test only has a "normal" difficulty with no extension.
+        }
+    },
+}
